64f2885c4cb0f463defca3fbbe4a812721c8cb00
remster85 <remi.desreumaux@gmail.com>
2025-02-09 15:20:04 -0500

    Update compare_table.sql

--- noreg/compare_table.sql
+++ noreg/compare_table.sql
@@ -16,12 +16,12 @@ BEGIN
     -- Generate JSON dynamically, EXCLUDING 'id' and timestamps
     SELECT string_agg(
         '    ' || quote_literal(column_name) || ': { ' ||
-        '    "t1_value": ' || 'COALESCE(''' || ' || quote_ident('t1.' || column_name) || '::TEXT' || ', ''NULL'')' || ', ' ||
-        '    "t2_value": ' || 'COALESCE(''' || ' || quote_ident('t2.' || column_name) || '::TEXT' || ', ''NULL'')' || ', ' ||
+        '    "t1_value": ' || 'COALESCE(quote_literal(t1.' || quote_ident(column_name) || '::TEXT), ''"NULL"'')' || ', ' ||
+        '    "t2_value": ' || 'COALESCE(quote_literal(t2.' || quote_ident(column_name) || '::TEXT), ''"NULL"'')' || ', ' ||
         '    "diff": ' ||
         '    CASE ' ||
-        '        WHEN t1.' || column_name || ' IS NULL AND t2.' || column_name || ' IS NULL THEN ''"NULL"'' ' ||
-        '        WHEN t1.' || column_name || ' = t2.' || column_name || ' THEN ''"MATCH"'' ' ||
+        '        WHEN t1.' || quote_ident(column_name) || ' IS NULL AND t2.' || quote_ident(column_name) || ' IS NULL THEN ''"NULL"'' ' ||
+        '        WHEN t1.' || quote_ident(column_name) || ' = t2.' || quote_ident(column_name) || ' THEN ''"MATCH"'' ' ||
         '        ELSE ''"DIFF"'' ' ||
         '    END ' ||
         '    }'
@@ -41,8 +41,8 @@ BEGIN
                 EXISTS (SELECT 1 FROM jsonb_each((''{' || json_text || '}''::jsonb)) 
                  WHERE value->>''diff'' = ''DIFF'') AS hasChanged,
                 (''{' || json_text || '}''::jsonb) AS changes
-         FROM ' || table1_name || ' t1
-         FULL JOIN ' || table2_name || ' t2 
+         FROM ' || quote_ident(table1_name) || ' t1
+         FULL JOIN ' || quote_ident(table2_name) || ' t2 
          ON t1.ref_id = t2.ref_id
          ORDER BY t1.ref_id';
 
